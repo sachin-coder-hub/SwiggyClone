76f743ff80a4be0a0ef0923513c7817832733de8
sachin-coder-hub <sachincoder0706@gmail.com>
2024-09-22 13:01:29 +0530

    Updated Contact us

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,30 +1,38 @@
 const Contact = () => {
   return (
-    <div>
-      <h1 className="font-bold text-2xl p-4 ">Contact Us</h1>
-      <p className="font-sans text-xl p-4">
-        Hi there! How we can help you today?
-      </p>
-      <form>
-        <input
-          type="text"
-          placeholder="Name"
-          className="border border-black p-2 m-2 rounded-lg"
-        />
-        <input
-          type="text"
-          placeholder="Email"
-          className="border border-black p-2 m-2 rounded-lg"
-        />
-        <input
-          type="text"
-          placeholder="Your Message"
-          className="border border-black p-2 m-2 rounded-lg"
-        />
-        <button className="border border-black p-2 m-2 bg-slate-900 text-white rounded-lg">
-          Submit
-        </button>
-      </form>
+    <div className="flex justify-center">
+      <div className="bg-orange-600 shadow-lg rounded-2xl m-4 p-4 w-full max-w-lg">
+        <h1 className="font-bold text-2xl p-4 text-center text-white">
+          Contact Us
+        </h1>
+        <p className="font-sans text-xl p-4 text-center text-white">
+          Hi there! How can we help you today?
+        </p>
+        <form className="flex flex-col items-center">
+          <input
+            type="text"
+            placeholder="Name"
+            className="border p-2 m-2 rounded-lg w-2/3"
+          />
+          <input
+            type="text"
+            placeholder="Email"
+            className="border p-2 m-2 rounded-lg w-2/3"
+          />
+          <input
+            type="text"
+            placeholder="Your Message"
+            className="border p-2 m-2 rounded-lg w-2/3"
+          />
+
+          {/* Center the submit button */}
+          <div className="w-full flex justify-center mt-4">
+            <button className="border border-black p-2 bg-slate-900 text-white rounded-lg w-1/2">
+              Submit
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
